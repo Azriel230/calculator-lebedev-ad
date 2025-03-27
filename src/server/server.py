bc438081fec545bef890bdea8d06d55b3426cd3b
milov-dk <106037729+Ne-ko-Chan@users.noreply.github.com>
2025-03-27 19:35:28 +0300

    GEAR-10: added history endpoint

--- src/server/server.py
+++ src/server/server.py
@@ -1,3 +1,4 @@
+import time
 from logger import LOGGER
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -12,6 +13,10 @@ app = FastAPI()
 class CalcBody(BaseModel):
     expression: str
 
+class HistoryInstance(BaseModel):
+    timestamp: float
+    expression: str
+    result: str
 
 @app.get("/")
 async def get_root():
@@ -26,3 +31,9 @@ async def post_calc_handler(body: CalcBody, float: str | None = None):
         isFloat = False
     res = calculate(body.expression, isFloat)
     return {"result": res}
+
+@app.get("/history")
+async def get_history():
+    mock_history = [HistoryInstance(timestamp=time.time(),expression="10+20",result="30"),HistoryInstance(timestamp=time.time()-1000,expression="10/20",result="0.5") ]
+    return mock_history
+
