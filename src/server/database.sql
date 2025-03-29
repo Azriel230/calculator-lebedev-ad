8ee7dd5ed92a7409a8477622f7da1716e55cc852
dyulkeeva-aa <yuumi@yandex.ru>
2025-03-29 21:08:51 +0300

    GEAR-11: Fixed bug. Added если-впадлу case

--- src/server/database.sql
+++
@@ -1,5 +0,0 @@
-
-
-
-create database calculator;
-create table history (timeReq bigint, expr varchar(255), result varchar(255));
--- src/server/server.py
+++ src/server/server.py
@@ -28,11 +28,14 @@ async def post_calc_handler(body: CalcBody, float: str | None = None):
 @app.get("/history")
 async def get_history():
     history=database.select_history();
+    #если впадлу поднимать,то юзайте
     #mock_history = [HistoryInstance(timestamp=10239238728, expression="10+20",result="30"),HistoryInstance(timestamp=10239238728-1000,expression="10/20",result="0.5") ]
     return history
 
 @app.post("/history")
 async def post_history(h:HistoryInstance):
+    #если впадлу вдвойне, то юзайте
+    #print(h)
     database.insert_history(h)
     return
 
