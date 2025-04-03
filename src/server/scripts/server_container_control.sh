525f2a8cd6c22b461e8e19b62af7a2da1b308ceb
milov-dk <106037729+Ne-ko-Chan@users.noreply.github.com>
2025-04-03 23:21:21 +0300

    GEAR-19: renamed image to match the pulled one

--- src/server/scripts/server_container_control.sh
+++ src/server/scripts/server_container_control.sh
@@ -1,9 +1,9 @@
 if [[ -z $(docker images -a | grep "server") ]]; then
-  docker build -t server .
+  docker build -t nekoch4n/calculator-server .
 fi
 
 if [[ -z $(docker ps -a | grep "server") ]]; then
-  docker run --network host --name server -d server
+  docker run --network host --name server -d nekoch4n/calculator-server
 else
   docker start server
 fi
